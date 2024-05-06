0db4a9f27c856572398ab678269959542592290d
rameenaamir137 <119052728+rameenaamir137@users.noreply.github.com>
2024-05-06 11:13:57 +0500

    Update notion_api.py

--- Notion-GoogleTasks-Sync/notion_api.py
+++ Notion-GoogleTasks-Sync/notion_api.py
@@ -1,7 +1,10 @@
 from notion.client import NotionClient
+from dotenv import load_dotenv
 
-# Constants
-NOTION_TOKEN = "v02%3Auser_token_or_cookies%3AaB3Rqo_Fqpu0dQdrv8A-_xrDd-8iwdJrBZ6k4DRNNIH4cO-Ul6lb4vMc-F_kBQ64dG8LNTgEgQF4tzzh93CFKEiPwvaJu9YBhFwPqOugX_sZkMB4TqyVXzaKkZtmNcE2P3Xq"
+# Load environment variables from .env file
+load_dotenv()
+# Read the NOTION_TOKEN from the environment
+NOTION_TOKEN = os.getenv("NOTION_TOKEN")
 
 # Authenticate and create Notion client
 def authenticate_notion():
